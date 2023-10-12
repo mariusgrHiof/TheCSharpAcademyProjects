85f1173fc5971e1e32acb5fe50177217d1a64618
Marius Gravningsmyhr <marius.gravningsmyhr@gmail.com>
2023-10-12 20:50:48 +0200

    First commit

---
+++ CodingTracker/CodingTracker/Models/CodingSession.cs
@@ -0,0 +1,32 @@
+﻿namespace CodingTracker.Models
+{
+    public class CodingSession
+    {
+
+        public int Id { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+
+
+
+        public CodingSession(DateTime startTime, DateTime endTime)
+        {
+            StartTime = startTime;
+            EndTime = endTime;
+        }
+
+
+        public Duration CalculateDuration()
+        {
+            TimeSpan timeSpan = EndTime - StartTime;
+
+            return new Duration
+            {
+                Hours = timeSpan.Hours,
+                Minutes = timeSpan.Minutes,
+            };
+
+        }
+
+    }
+}
