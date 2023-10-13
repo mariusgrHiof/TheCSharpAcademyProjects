c63d73b0f369eebf834b20bf6743086edfd6c16c
Marius Gravningsmyhr <marius.gravningsmyhr@gmail.com>
2023-10-13 13:47:48 +0200

    Add validation

---
+++ CodingTracker/CodingTracker/Utils/Validation.cs
@@ -0,0 +1,12 @@
+﻿using System.Globalization;
+
+namespace CodingTracker.Utils
+{
+    public static class Validation
+    {
+        public static bool ValidateDate(string date)
+        {
+            return DateTime.TryParseExact(date, "dd/MM/yyyy HH:mm", new CultureInfo("nb-NO"), DateTimeStyles.None, out _);
+        }
+    }
+}
