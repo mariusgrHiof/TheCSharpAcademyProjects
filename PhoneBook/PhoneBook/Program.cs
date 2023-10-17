707fff5f25a25036e429dbbe7750d17151bf407c
Marius Gravningsmyhr <marius.gravningsmyhr@gmail.com>
2023-10-17 16:22:04 +0200

    Initial commit

---
+++ PhoneBook/PhoneBook/Program.cs
@@ -0,0 +1,32 @@
+﻿
+using PhoneBook.Data;
+using PhoneBook.Models;
+using PhoneBook.Utils;
+
+PhoneBookDbContext context = new PhoneBookDbContext();
+
+PhoneBookRepository db = new PhoneBookRepository(context);
+
+List<Contact> contacts = db.GetContacts();
+
+foreach (var contact in contacts)
+{
+    Console.WriteLine(contact.Name);
+}
+
+string email = "update@gmail.com";
+
+if (!Validate.IsValidEmail(email))
+{
+    Console.WriteLine("Invalid email");
+}
+else
+{
+    db.UpdateContact(5, new Contact
+    {
+        Id = 5,
+        Name
+        Email = email,
+        PhoneNumber = "43908543895"
+    });
+}
