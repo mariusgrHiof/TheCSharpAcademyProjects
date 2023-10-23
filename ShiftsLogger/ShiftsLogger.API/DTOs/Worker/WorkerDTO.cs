1acd403467344e5cf164d18a89d7748397ed56f9
Marius Gravningsmyhr <marius.gravningsmyhr@gmail.com>
2023-10-23 19:24:14 +0200

    renamed class
    refactor and added get all workers method
    
    Change return type to GetWorkDTO
    
    Add json propertyname

--- ShiftsLogger/ShiftsLogger.API/DTOs/Worker/WorkerDTO.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace ShiftsLogger.API.DTOs.Worker;
-public class WorkerDTO
-{
-    public int Id { get; set; }
-    public string FirstName { get; set; } = string.Empty;
-    public string LastName { get; set; } = string.Empty;
-}
