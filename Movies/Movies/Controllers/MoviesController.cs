d4712734d15adb9755d33ecab6384824633c92fa
Marius Gravningsmyhr <marius.gravningsmyhr@gmail.com>
2023-10-31 22:45:04 +0100

    initial commit

---
+++ Movies/Movies/Controllers/MoviesController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Movies.Models;
+
+namespace Movies.Controllers
+{
+    public class MoviesController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Movie> movies = new List<Movie>()
+            {
+            new Movie
+            {
+                Id = 1,
+                Title = "Rocky 4",
+                Year = 1985,
+                Genre = "Drama",
+                DurationInMinutes = 90
+            },
+             new Movie
+            {
+                Id = 2,
+                Title = "Rocky 3",
+                Year = 1983,
+                Genre = "Drama",
+                DurationInMinutes = 75
+            }
+        };
+            return View(movies);
+        }
+    }
+}
