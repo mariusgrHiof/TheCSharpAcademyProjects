c6afaeb353919ce7f1caf0d2a5ef99e61bd2c5cb
Marius Gravningsmyhr <marius.gravningsmyhr@gmail.com>
2023-11-02 00:05:49 +0100

    add db,create and edit

--- Movies/Movies/Controllers/MoviesController.cs
+++ Movies/Movies/Controllers/MoviesController.cs
@@ -1,32 +1,74 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Movies.Data;
 using Movies.Models;
 
 namespace Movies.Controllers
 {
     public class MoviesController : Controller
     {
-        public IActionResult Index()
+        private readonly ApplicationDbContext _context;
+
+        public MoviesController(ApplicationDbContext context)
         {
-            List<Movie> movies = new List<Movie>()
-            {
-            new Movie
-            {
-                Id = 1,
-                Title = "Rocky 4",
-                Year = 1985,
-                Genre = "Drama",
-                DurationInMinutes = 90
-            },
-             new Movie
-            {
-                Id = 2,
-                Title = "Rocky 3",
-                Year = 1983,
-                Genre = "Drama",
-                DurationInMinutes = 75
-            }
-        };
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            List<Movie> movies = await _context.Movies.ToListAsync();
+
             return View(movies);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Title,Year,Genre,DurationInMinutes")] Movie movie)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Movies.Add(movie);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Movies == null)
+            {
+                return NotFound();
+            }
+
+            var movie = await _context.Movies.FindAsync(id);
+            if (movie == null)
+            {
+                return NotFound();
+            }
+
+            return View(movie);
+        }
+
+        [HttpPost("{id}")]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Title,Year,Genre,DurationInMinutes")] Movie movie)
+        {
+            if (!ModelState.IsValid)
+            {
+                _context.Movies.Update(movie);
+                await _context.SaveChangesAsync();
+
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View();
         }
     }
 }
