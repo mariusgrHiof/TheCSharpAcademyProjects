0aa35c36eb8d349c9c8aa3769eb8eeab0e74c477
Marius Gravningsmyhr <marius.gravningsmyhr@gmail.com>
2023-11-09 13:39:28 +0100

    Add html and js files

---
+++ TodoApp/TodoApp/wwwroot/js/script.js
@@ -0,0 +1,23 @@
+﻿console.log('Program started');
+
+function getTodos() {
+    console.log('Getting todos...');
+   //Send request til server
+    // Vis data ved hjelp av displayItems function
+    fetch('api/todoItems')
+        .then(response => response.json())
+        .then(data => displayItems(data));
+}
+
+function displayItems(data) {
+    const body = document.getElementById('body');
+    const ul = document.createElement('ul');
+
+    for (var i = 0; i < data.length; i++) {
+        const { id, name, isComplete } = data[i];
+        const li = document.createElement('li');
+        li.textContent = `${id} ${name} ${isComplete}`;
+        ul.appendChild(li);
+    }
+    body.appendChild(ul);
+ }
