a44bafb496a6b41c8e22fbc07d21302611028d40
Marius Gravningsmyhr <marius.gravningsmyhr@gmail.com>
2023-11-14 23:55:30 +0100

    Add user interface

--- DrinksInfo/DrinksInfo/Program.cs
+++ DrinksInfo/DrinksInfo/Program.cs
@@ -1,5 +1,6 @@
 ﻿using DrinksInfo.Api;
 using DrinksInfo.Controllers;
+using DrinksInfo.UI;
 
 HttpClient client = new HttpClient()
 {
@@ -9,39 +10,6 @@ ApiClient apiClient = new ApiClient(client);
 
 DrinksController drinksController = new DrinksController(apiClient);
 
-var categories = await drinksController.GetCategoriesAsync();
+UserInterface ui = new UserInterface(drinksController);
 
-Console.WriteLine("Categories");
-Console.WriteLine("--------------------");
-foreach (var category in categories)
-{
-    Console.WriteLine(category.CategoryName);
-}
-Console.WriteLine("--------------------");
-
-Console.WriteLine("Drink detail id: 11007");
-Console.WriteLine("--------------------");
-
-var drink = await drinksController.GetDrinkByIdAsync("110071");
-if (drink == null)
-{
-    Console.WriteLine("Drink not found");
-    Console.WriteLine("--------------------");
-
-}
-else
-{
-    Console.WriteLine(drink.DrinkName);
-    Console.WriteLine("--------------------");
-}
-
-var drinksByCategoryName = await drinksController.GetDrinksByCategoryAsync("Punch / Party Drink");
-
-Console.WriteLine("Drinks by gatecory name: Punch / Party Drink");
-Console.WriteLine("--------------------");
-
-foreach (var drinkNyCategory in drinksByCategoryName)
-{
-    Console.WriteLine(drinkNyCategory.DrinkName);
-}
-Console.WriteLine("--------------------");
+await ui.Run();
