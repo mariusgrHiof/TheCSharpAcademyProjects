a33a387e6b8eeea2573f9fb352717961885195c5
Marius Gravningsmyhr <marius.gravningsmyhr@gmail.com>
2023-11-15 11:43:13 +0100

    Add properties

--- HabitLogger/HabitLogger/CSharpLog.cs
+++ HabitLogger/HabitLogger/CSharpLog.cs
@@ -3,6 +3,8 @@
     public class CSharpLog
     {
         public int Id { get; set; }
+        public DateTime DateCreated { get; set; } = DateTime.Now;
+        public DateTime DateUpdated { get; set; } = DateTime.Now;
         public int Hours { get; set; }
     }
 }
