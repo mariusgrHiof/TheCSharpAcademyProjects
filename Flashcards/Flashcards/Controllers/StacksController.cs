8e94432c43ad916ee500f8e426ff4ee6605fc009
Marius Gravningsmyhr <marius.gravningsmyhr@gmail.com>
2023-11-16 16:24:50 +0100

    Add stack and flashcard actions

---
+++ Flashcards/Flashcards/Controllers/StacksController.cs
@@ -0,0 +1,82 @@
+﻿using Flashcards.Data;
+using Flashcards.Models;
+
+namespace Flashcards.Controllers;
+
+public class StacksController
+{
+    private readonly DataAccess _dataAccess;
+
+    public StacksController(DataAccess dataAccess)
+    {
+        _dataAccess = dataAccess;
+    }
+
+    public async Task<List<Stack>> GetStacksAsync()
+    {
+        return await _dataAccess.GetAllStacksAsync();
+    }
+
+    public async Task<Stack> GetStackByIdAsync(int id)
+    {
+        var stack = await _dataAccess.GetStackByIdAsync(id);
+        if (stack == null)
+        {
+            return null;
+        }
+
+        return stack;
+    }
+
+    public async Task<Stack> UpdateStackByIdAsync(int id, Stack updateStack)
+    {
+        var stack = await _dataAccess.UpdateStackByIdAsync(id, updateStack);
+        if (stack == null)
+        {
+            return null;
+        }
+
+        return stack;
+    }
+
+    public async Task<Stack> DeleteStackByIdAsync(int id)
+    {
+        var stack = await _dataAccess.DeleteStackByIdAsync(id);
+        if (stack == null) return null;
+
+        return stack;
+    }
+
+    public async Task<Stack> AddStackAsync(Stack newStack)
+    {
+        var stack = await _dataAccess.AddStackAsync(newStack);
+        if (stack == null) return null;
+
+        return stack;
+    }
+
+    public async Task<Flashcard> AddFlashcardAsync(int stackId, Flashcard newFlashcard)
+    {
+        var flashcard = await _dataAccess.AddFlashcardAsync(stackId, newFlashcard);
+        if (flashcard == null) return null;
+
+        return flashcard;
+    }
+
+    public async Task<Flashcard> UpdateFlashcardById(int stackId, Flashcard updateFlashcard)
+    {
+        var flashcard = await _dataAccess.UpdateFlashcardAsync(stackId, updateFlashcard);
+        if (flashcard == null) return null;
+
+        return flashcard;
+    }
+
+    public async Task<Flashcard> DeleteFlashcardById(int stackId, int flashcardId)
+    {
+        var flashcard = await _dataAccess.DeleteFlashcardAsync(stackId, flashcardId);
+        if (flashcard == null) return null;
+
+        return flashcard;
+    }
+}
+
---
+++ Flashcards/Flashcards/Data/DataAccess.cs
@@ -0,0 +1,209 @@
+﻿using Flashcards.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Flashcards.Data;
+public class DataAccess
+{
+    private readonly FlashcardsDbContext _context;
+
+    public DataAccess(FlashcardsDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<List<Stack>> GetAllStacksAsync()
+    {
+        return await _context.Stacks.ToListAsync();
+    }
+    public async Task<List<Stack>> GetAllStacksWithFlashcardsAsync()
+    {
+        return await _context.Stacks
+            .Include(s => s.Flashcards)
+            .ToListAsync();
+    }
+
+    public async Task<Stack> GetStackByIdAsync(int id)
+    {
+        var stack = await _context.Stacks.FirstOrDefaultAsync(s => s.Id == id);
+        if (stack == null)
+        {
+            return null;
+        }
+
+        return stack;
+    }
+    public async Task<Stack> GetStackWithFlashcardsByIdAsync(int id)
+    {
+        var stack = await _context.Stacks
+            .Include(s => s.Flashcards)
+            .FirstOrDefaultAsync(s => s.Id == id);
+        if (stack == null)
+        {
+            return null;
+        }
+
+        return stack;
+    }
+
+    public async Task<Stack> UpdateStackByIdAsync(int id, Stack Updatestack)
+    {
+        if (Updatestack == null) return null;
+        if (Updatestack.Id != id) return null;
+
+        var stack = await _context.Stacks.FirstOrDefaultAsync(stack => stack.Id == id);
+
+        if (stack == null)
+        {
+            return null;
+        }
+
+        stack.Id = Updatestack.Id;
+        stack.Name = Updatestack.Name;
+
+        _context.Stacks.Update(stack);
+
+        try
+        {
+            await _context.SaveChangesAsync();
+
+            return stack;
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Fail to update record. Details: ", ex.Message);
+
+        }
+        return stack;
+    }
+
+    public async Task<Stack> DeleteStackByIdAsync(int id)
+    {
+        var stack = await _context.Stacks.FirstOrDefaultAsync(s => s.Id == id);
+        if (stack == null) return null;
+
+        _context.Stacks.Remove(stack);
+        try
+        {
+            await _context.SaveChangesAsync();
+            return stack;
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Fail to delete record. Details: {ex.Message}");
+            return null;
+        }
+    }
+
+    public async Task<Stack> AddStackAsync(Stack stack)
+    {
+        if (stack == null) return null;
+
+        _context.Stacks.Add(stack);
+
+        try
+        {
+            await _context.SaveChangesAsync();
+
+            return stack;
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Fail to add record.Details: {ex.Message}");
+            return null;
+        }
+    }
+
+    public async Task<Flashcard> AddFlashcardAsync(int stackId, Flashcard flashcard)
+    {
+        if (flashcard == null) return null;
+        var stack = await _context.Stacks
+            .Include(s => s.Flashcards)
+            .FirstOrDefaultAsync(s => s.Id == stackId);
+
+        if (stack == null) return null;
+
+        stack.Flashcards.Add(flashcard);
+
+        try
+        {
+            await _context.SaveChangesAsync();
+            return flashcard;
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Fail to add record.Details: {ex.Message}");
+            return null;
+        }
+    }
+
+    public async Task<Flashcard> UpdateFlashcardAsync(int stackId, Flashcard updateFlashcard)
+    {
+        Flashcard flashcard = null;
+
+        if (updateFlashcard == null) return null;
+        var stack = await _context.Stacks
+            .Include(s => s.Flashcards)
+            .FirstOrDefaultAsync(s => s.Id == stackId);
+
+        if (stack != null)
+        {
+            flashcard = stack.Flashcards.FirstOrDefault(fc => fc.Id == updateFlashcard.Id);
+            if (flashcard == null) return null;
+
+            flashcard.Question = updateFlashcard.Question;
+            flashcard.Answer = updateFlashcard.Answer;
+
+            var index = stack.Flashcards.IndexOf(flashcard);
+
+            if (index != -1)
+            {
+                stack.Flashcards[index] = flashcard;
+            }
+            else
+            {
+                Console.WriteLine("Phone number not found.");
+            }
+        }
+
+        _context.Stacks.Update(stack);
+
+        try
+        {
+            await _context.SaveChangesAsync();
+            return flashcard;
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Fail to add record.Details: {ex.Message}");
+            return null;
+        }
+    }
+
+    public async Task<Flashcard> DeleteFlashcardAsync(int stackId, int flashcardId)
+    {
+
+        var stack = await _context.Stacks
+            .Include(s => s.Flashcards)
+            .FirstOrDefaultAsync(s => s.Id == stackId);
+
+        if (stack == null) return null;
+
+        var flashcard = stack.Flashcards.FirstOrDefault(f => f.Id == flashcardId);
+        if (flashcard == null) return null;
+
+        stack.Flashcards.Remove(flashcard);
+
+        try
+        {
+            await _context.SaveChangesAsync();
+            return flashcard;
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Fail to delete record.Details: {ex.Message}");
+            return null;
+        }
+    }
+
+}
+
