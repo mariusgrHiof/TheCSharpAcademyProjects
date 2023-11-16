b83d1687aad442095858055503969ce5098c2312
Marius Gravningsmyhr <marius.gravningsmyhr@gmail.com>
2023-11-16 10:28:41 +0100

    Add models

---
+++ Flashcards/Flashcards/Models/Stack.cs
@@ -0,0 +1,10 @@
+﻿namespace Flashcards.Models;
+
+public class Stack
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+
+    public List<Flashcard> Flashcards { get; set; }
+}
+
