99a130a2d63c4166af8fa3181b9a4a7d15f911c0
Marius Gravningsmyhr <marius.gravningsmyhr@gmail.com>
2024-01-19 12:41:31 +0100

    Add category and transaction models

---
+++ Budget/MVCBudget.mariusgrHiof/Models/Transaction.cs
@@ -0,0 +1,10 @@
+﻿namespace MVCBudget.mariusgrHiof.Models;
+
+public class Transaction
+{
+    public int Id { get; set; }
+    public DateTime Date { get; set; }
+    public Decimal Amount { get; set; }
+    public int CategoryId { get; set; }
+    public Category Category { get; set; }
+}
