2cc454910624e12fa64a2ae2bfe0bb8c7f6c09e9
Marius Gravningsmyhr <marius.gravningsmyhr@gmail.com>
2024-01-22 22:07:44 +0100

    Delete Budget directory

--- Budget/MVCBudget.mariusgrHiof/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace MVCBudget.mariusgrHiof.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
