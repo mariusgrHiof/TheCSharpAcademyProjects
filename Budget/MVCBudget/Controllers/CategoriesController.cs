78045ce6f4f2177d55029121c87fdf7b94579554
Marius Gravningsmyhr <marius.gravningsmyhr@gmail.com>
2024-01-25 14:43:54 +0100

    Add categories controller

---
+++ Budget/MVCBudget/Controllers/CategoriesController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MVCBudget.Data;
+using MVCBudget.Dtos;
+
+namespace MVCBudget.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class CategoriesController : ControllerBase
+{
+    private readonly BudgetContext _context;
+
+    public CategoriesController(BudgetContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet("GetCategories")]
+    public async Task<IActionResult> GetCategories()
+    {
+        var categories = await _context.Categories.ToListAsync();
+        var categoriesDtos = categories.Select(c => new CategoryDto
+        {
+            Id = c.Id,
+            Name = c.Name,
+        });
+        return Ok(categoriesDtos);
+    }
+}
